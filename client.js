fde979a039c095786272799930ab1b7029703860
Pramod L <pramodliv1@gmail.com>
2023-09-15 10:10:47 +0530

    third commit

--- client.js
+++ client.js
@@ -1,3 +1,10 @@
 const fetch = require("node-fetch");
 
+async function main() {
+  const response = await fetch("http://localhost:3000/todos?user_id=2");
+  const data = await response.json();
+  console.log(data);
+}
+main();
+
 // write your code here
